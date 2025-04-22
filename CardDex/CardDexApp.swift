702bdcf2efe0044b4a4e6e5219f80641320af92d
Yiwei04 <jefferywang812@gmail.com>
2025-04-22 12:55:34 +1000

    Initial Commit

---
+++ CardDex/CardDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardDexApp.swift
+//  CardDex
+//
+//  Created by Jeffery Wang on 22/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct CardDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
